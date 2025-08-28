a158a6a734a71bb227b3fdd9d343ad682ec09d4a
pushpaanand <productananlyst.pushpa@kauveryhospital.com>
2025-08-28 16:53:12 +0530

    base url and build file

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(cors(corsOptions));
 // Body parsing middleware
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
-// app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 // Request logging middleware
 app.use((req, res, next) => {
@@ -203,9 +203,9 @@ if (NODE_ENV === 'development') {
   });
 }
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client","build", "index.html"));
+});
 
 // Start server
 app.listen(PORT, () => {
